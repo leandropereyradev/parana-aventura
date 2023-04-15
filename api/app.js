57b2c2743773d7ac28e0263cb25db48715f47304
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-04-16 00:35:57 +0200

    MongoDB configutation

--- api/app.js
+++ api/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const logger = require("morgan");
 
+require("./config/db.config");
 const app = express();
 
 app.use(logger("dev"));
