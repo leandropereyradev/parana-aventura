2616b579b43f7580856fc96d63568d9bab08f30c
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-04-16 18:00:54 +0200

    User CRUD Routes and Controllers

--- api/app.js
+++ api/app.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 const createError = require("http-errors");
 
 require("./config/db.config");
 const app = express();
 
+app.use(express.json());
 app.use(logger("dev"));
 
 app.use("/api", require("./config/routes.config"));
@@ -18,11 +20,28 @@ app.use("/api", require("./config/routes.config"));
 app.use((req, res, next) => next(createError(404, "Route not found")));
 
 app.use((error, req, res, next) => {
-  if (!error.status) error = createError(500, error);
+  if (error instanceof mongoose.Error.ValidationError)
+    error = createError(400, error);
+  else if (error instanceof mongoose.Error.CastError && error.path === "_id")
+    error = createError(4.4, "Source not found");
+  else if (!error.status) error = createError(500, error);
 
   const data = {
     message: error.message,
   };
+
+  if (error.errors) {
+    const errors = Object.keys(error.errors).reduce(
+      (errorsObject, errorKey) => {
+        errorsObject[errorKey] = error.errors[errorKey].message;
+
+        return errorsObject;
+      },
+      {}
+    );
+
+    data.errors = errors;
+  }
 
   res.status(error.status).json(data);
 });
--- api/controllers/users.controller.js
+++ api/controllers/users.controller.js
@@ -1,15 +1,38 @@
+const Users = require("../models/user.model");
+const createError = require("http-errors");
+
 module.exports.register = (req, res, next) => {
-  res.json([]);
+  Users.create(req.body)
+    .then((user) => res.status(201).json(user))
+    .catch(next);
 };
 
 module.exports.detail = (req, res, next) => {
-  res.json([]);
+  Users.findById(req.params.id)
+    .then((user) => {
+      if (!user) next(createError(404, "User not found"));
+      else res.json(user);
+    })
+    .catch(next);
 };
 
 module.exports.update = (req, res, next) => {
-  res.json([]);
+  Users.findByIdAndUpdate(req.params.id, req.body, {
+    runValidators: true,
+    new: true,
+  })
+    .then((user) => {
+      if (!user) next(createError(404, "User not found"));
+      else res.json(user);
+    })
+    .catch(next);
 };
 
 module.exports.delete = (req, res, next) => {
-  res.json([]);
+  Users.findByIdAndDelete(req.params.id)
+    .then((user) => {
+      if (!user) next(createError(404, "User not found"));
+      else res.status(204).send();
+    })
+    .catch(next);
 };
