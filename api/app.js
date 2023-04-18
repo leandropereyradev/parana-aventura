e05d62234eea933cec20e3a239ff7da1f542af3a
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-04-18 12:10:55 +0200

    Removing the error module to a Middleware

--- api/app.js
+++ api/app.js
@@ -14,44 +14,9 @@ app.use(logger("dev"));
 
 app.use("/api", require("./config/routes.config"));
 
-// Middleware space
-
-//
-
 app.use((req, res, next) => next(createError(404, "Route not found")));
 
-app.use((error, req, res, next) => {
-  if (error instanceof mongoose.Error.ValidationError)
-    error = createError(400, error);
-  else if (error instanceof mongoose.Error.CastError && error.path === "_id") {
-    const sourseName = error.model().constructor.modelName;
-    error = createError(404, `${sourseName} not found`);
-  } else if (error.message.includes("E11000")) {
-    // Object.keys(error.keyValue).forEach(
-    //   (key) => (error.keyValue[key] = "This mail already exists")
-    // );
-    error = createError(409, "This email already exists");
-  } else if (!error.status) error = createError(500, error);
-
-  const data = {
-    message: error.message,
-  };
-
-  if (error.errors) {
-    const errors = Object.keys(error.errors).reduce(
-      (errorsObject, errorKey) => {
-        errorsObject[errorKey] = error.errors[errorKey].message;
-
-        return errorsObject;
-      },
-      {}
-    );
-
-    data.errors = errors;
-  }
-
-  res.status(error.status).json(data);
-});
+app.use(require("./middleware/errors.mid"));
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () =>
---
+++ api/middleware/errors.mid.js
@@ -0,0 +1,37 @@
+const createError = require("http-errors");
+const mongoose = require("mongoose");
+
+const errors = (error, req, res, next) => {
+  if (error instanceof mongoose.Error.ValidationError)
+    error = createError(400, error);
+  else if (error instanceof mongoose.Error.CastError && error.path === "_id") {
+    const sourseName = error.model().constructor.modelName;
+    error = createError(404, `${sourseName} not found`);
+  } else if (error.message.includes("E11000")) {
+    // Object.keys(error.keyValue).forEach(
+    //   (key) => (error.keyValue[key] = "This mail already exists")
+    // );
+    error = createError(409, "This email already exists");
+  } else if (!error.status) error = createError(500, error);
+
+  const data = {
+    message: error.message,
+  };
+
+  if (error.errors) {
+    const errors = Object.keys(error.errors).reduce(
+      (errorsObject, errorKey) => {
+        errorsObject[errorKey] = error.errors[errorKey].message;
+
+        return errorsObject;
+      },
+      {}
+    );
+
+    data.errors = errors;
+  }
+
+  res.status(error.status).json(data);
+};
+
+module.exports = errors;
